c227502ae209732044d94ae3f9bfdbda89491f89
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2024-10-12 19:04:05 +0400

    newwww

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   const logger = new Logger('NestApplication');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(port, () => {
     logger.log(`Server is running on port ${port}`);
   });
